a2692c62fa62ba9d702c5774c544f7117c882c18
komalbhayana27 <komal.bhayana@student.csulb.edu>
2017-06-06 20:03:38 -0500

    Add files via upload

---
+++ NewApiDataApplication.java
@@ -0,0 +1,12 @@
+package io.komal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewApiDataApplication.class, args);
+	}
+}
